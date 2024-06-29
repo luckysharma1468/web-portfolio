7d5012d37ff206a0cb468b2e2f011faa969954b6
Prashant Sharma <sharmalucky14998@gmail.com>
2024-06-29 23:23:11 +0530

    Updates

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import "../css/footer.css"
-
-export default function Footer() {
-  return (
-    <div className="d-flex justify-content-center mt-5 myfooter py-3">
-        Made with 🤍 by Prashant Sharma
-    </div>
-  )
-}
